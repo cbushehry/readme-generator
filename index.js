2312992db04af13f528fee65b967e23d0515ffe0
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-10-03 15:29:13 -0700

    index.js v2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {writeFile} = require('./utils/generate-readme-md.js');
 
 
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -85,12 +85,93 @@ const promptUser = () => {
     ]);
 }
 
+const promptProjectTips = readMeAnswers => {
+    // If there's no 'projects' array property, create one
+   if (!readMeAnswers.projects) {
+      readMeAnswers.projects = [];
+   }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  return inquirer.prompt([
 
-// TODO: Create a function to initialize app
-function init() {}
+      {
+          type: 'input',
+          name: 'addTip',
+          message: 'Provide a description on how to achieve a the step/tip',
+        },
+      {
+          type: 'input',
+          name: 'addPicture',
+          message: 'Provide a snapshot name of step if applicable?',
+      },
+        {
+          type: 'confirm',
+          name: 'tipConfirmation',
+          message: 'Would you like to add any other tips to your project?',
+          default: false
+      },
+  ]).then(projectData => {
+      readMeAnswers.projects.push(projectData);
+      if (projectData.tipConfirmation) {
+        return promptProjectTips(readMeAnswers);
+      } else {
+        return readMeAnswers;
+      }
+    });
+};
 
-// Function call to initialize app
-init();
+// *** Test Data *** //
+
+const testData = 
+{
+  gitHubUser: 'test user',
+  emailAddress: "name@email.com",
+  projectTitle: 'Test Title',
+  desc: 'Test Description',
+  webAddress: 'https://github.com/username/project-title',
+  featuresGif: 'new-tab',
+  credits: 'Thanks ---- ---- for working on this project.',
+  licenses: [ 'mit' ],
+  commandInstall: 'npm i',
+  commandTest: 'node app.js test',
+  contrDesc: 'email ---- ---- - name@email.com',
+  projects: [
+    {
+      addTip: 'test step 1',
+      addPicture: 'test',
+      tipConfirmation: true
+    },
+    {
+      addTip: 'test step 2',
+      addPicture: 'test',
+      tipConfirmation: false
+    }
+  ]
+};
+
+
+
+// *** Check if we are in testing envrioment *** //
+
+const profileDataArgs = process.argv.slice(2);
+
+if (profileDataArgs.length === 0) {
+
+  promptUser()
+  .then(promptProjectTips)
+  .then(answers => {
+      console.log(answers)
+      return generateReadMe(answers);
+  })
+  .then(readmeFile => {
+      //console.log(readmeFile);
+      return writeFile(readmeFile)
+  })
+  .catch(err => {
+  console.log(err);
+  });
+
+} else if ( ( profileDataArgs.length > 0) && (profileDataArgs[0].toLowerCase() === "test") )  {
+  //console.log(profileDataArgs);
+  console.log(generateReadMe(testData))
+
+};
