a36acd81bc8dc138be73f11c334fd53958ce04f7
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-10-03 15:40:00 -0700

    added src folder, and readme-template.js v1

---
+++ src/readme-template.js
@@ -0,0 +1,14 @@
+const generateReadMe = templateData => {
+
+    // const {projectTitle, desc, webAddress,
+    // featuresGif, credits, licenses, commandInstall , commandTest, contrDesc, ...tips } = templateData;
+
+    
+    const {gitHubUser, emailAddress, projectTitle, desc, webAddress,
+        featuresGif, credits, licenses, commandInstall , commandTest, contrDesc, projects } = templateData;
+
+       // console.log(templateData);
+        //console.log(tips);
+
+     const genereateTips = tips => {
+        // console.log(tips)
