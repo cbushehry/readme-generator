5fdac5f0568bd77e9a6a67d86ca3eaf58a44e9ae
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-10-03 19:13:10 -0700

    index.js v4, readme-template finished

--- index.js
+++ index.js
@@ -52,11 +52,6 @@ const promptUser = () => {
           },
           {
             type: 'input',
-            name: 'featuresGif',
-            message: 'Please include a gif video (use name of gif) showcasing all your features if applicable? e.g new-feature',
-          },
-          {
-            type: 'input',
             name: 'credits',
             message: 'Include credits if applicable'
           },
@@ -64,8 +59,7 @@ const promptUser = () => {
             type: 'checkbox',
             name: 'licenses',
             message: 'What kind of license should your project have? (Check all that apply)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-            //choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+            choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
           },
           {
             type: 'input',
@@ -77,11 +71,6 @@ const promptUser = () => {
             name: 'commandTest',
             message: 'What command should be run to run tests (output will be console.log)?'
           },
-          {
-            type: 'input',
-            name: 'contrDesc',
-            message: 'What does the user need to know about contributing to the repo?'
-          }
     ]);
 }
 
@@ -148,9 +137,9 @@ const testData =
   ]
 };
 
-
-
 // *** Check if we are in testing envrioment *** //
+
+
 
 const profileDataArgs = process.argv.slice(2);
 
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,11 +1,154 @@
-const generateReadMe = templateData => {
+//  *** Incoming templateData sample *** //
+/*
+{
+    projectTitle: 'Test',
+    desc: 'Test',
+    webAddress: 'Test',
+    usageTips: 'Test',
+    usageTipsPicture: false,
+    featuresGif: 'Test',
+    credits: 'Test',
+    licenses: [ 'afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0' ],
+    commandInstall: 'Test',
+    commandTest: 'Test',
+    contrDesc: 'Test'
+  }
+    projects: [
+    { addTip: 'test', addPicture: 'test', tipConfirmation: true },
+    { addTip: 'test1234', addPicture: '', tipConfirmation: false }
+*/
 
+
+const generateReadMe = templateData => {
     
     const {gitHubUser, emailAddress, projectTitle, desc, webAddress,
         featuresGif, credits, licenses, commandInstall , commandTest, contrDesc, projects } = templateData;
 
+
+    const licensesBadges = licensesArr => {
+
+        // const licensesValues = ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 
+        // 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 
+        // 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 
+        // 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+
+        let outputLicenses = [];
+
+
+        for (i = 0; i < licensesArr.length; i++) {
+
+            switch(licensesArr[i]) {
+                case "afl-3.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-AFL%203.0-blue.svg)](https://opensource.org/licenses/afl-3.0)");
+                    break;
+                case "apache-2.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)");
+                    break;
+                case "artistic-2.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-Artistic%202.0-blue.svg)](https://opensource.org/licenses/artistic-2.0)");
+                    break;
+                case "bsl-1.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-BSL%201.0-blue.svg)](https://opensource.org/licenses/bsl-1.0)");
+                    break;
+                case "bsd-2-clause":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-BSD%202--Clause-blue.svg)](https://opensource.org/licenses/BSD-2-Clause)");
+                   break;
+                case "bsd-3-clause":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)");
+                    break;
+                case "bsd-3-clause-clear":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-BSD%203--ClauseClear-blue.svg)](https://opensource.org/licenses/bsd-3-clause-clear)");
+                    break;
+                case "cc":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-CC%20-blue.svg)](https://opensource.org/licenses/cc)");
+                    break;
+                case "cc0-1.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-CC0%201.0-blue.svg)](http://creativecommons.org/publicdomain/zero/1.0/)");
+                    break;
+                case "cc-by-4.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-CC%20BY%204.0-blue.svg)](https://creativecommons.org/licenses/by/4.0/)");
+                    break;
+                case "wtfpl":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-WTFPL-blue.svg)](http://www.wtfpl.net/about/)");
+                    break;
+                case "ecl-2.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-ECL%202.0-blue.svg)](https://opensource.org/licenses/ecl-2.0)");
+                    break;
+                case "epl-1.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-ECL%201.0-blue.svg)](https://opensource.org/licenses/epl-1.0)");
+                    break;
+                case "epl-2.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-EPL%202.0-blue.svg)](https://opensource.org/licenses/epl-2.0)");
+                    break;
+                case "eupl-1.1":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-EUPL%201.1-blue.svg)](https://opensource.org/licenses/eupl-1.1)");
+                    break;
+                case "agpl-3.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-AGPL%203.0-blue.svg)](https://opensource.org/licenses/agpl-3.0)");
+                    break;
+                case "gpl":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-GPL%20-blue.svg)](https://opensource.org/licenses/gpl)");
+                    break;
+                case "gpl-2.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-GPL%202.0-blue.svg)](https://opensource.org/licenses/gpl-2.0)");
+                    break;
+                case "gpl-3.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://opensource.org/licenses/gpl-3.0)");
+                    break;
+                case "lgpl":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-LGPL%20-blue.svg)](https://opensource.org/licenses/lgpl)");
+                    break;
+                case "lgpl-2.1":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-LGPL%202.1-blue.svg)](https://opensource.org/licenses/lgpl-2.1)");
+                    break;
+                case "lgpl-3.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-LGPL%203.0-blue.svg)](https://opensource.org/licenses/lgpl-3.0)");
+                    break;
+                case "isc":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-ISC%20-blue.svg)](https://opensource.org/licenses/isc)");
+                    break;
+                case "lppl-1.3c":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-IPPL%201.3c-blue.svg)](https://opensource.org/licenses/lppl-1.3c)");
+                    break;
+                case "ms-pl":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-MSPL%20-blue.svg)](https://opensource.org/licenses/ms-pl)");
+                    break;
+                case "mit":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-MIT%20-blue.svg)](https://opensource.org/licenses/mit)");
+                    break;
+                case "mpl-2.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-MPL%202.0-blue.svg)](https://opensource.org/licenses/mpl-2.0)");
+                    break;
+                case "osl-3.0":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-OSL%203.0-blue.svg)](https://opensource.org/licenses/osl-3.0)");
+                    break;
+                case "postgresql":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-POSTGRESQL%20-blue.svg)](https://opensource.org/licenses/postgresql)");
+                    break;
+                case "ofl-1.1":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-OFL%201.1-blue.svg)](https://opensource.org/licenses/ofl-1.1)");
+                    break;
+                case "ncsa":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-NCSA%20-blue.svg)](https://opensource.org/licenses/ncsa)");
+                    break;
+                case "unlicense":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-UNLICENSE%20-blue.svg)](https://opensource.org/licenses/unlicense)");
+                    break;
+                case "zlib":
+                    outputLicenses.push("[![License](https://img.shields.io/badge/License-ZLIB%20-blue.svg)](https://opensource.org/licenses/zlib)");
+                    break;
+                default:
+                    // Do nothing - no selected licenses
+            };
+        };
+        //console.log(outputLicenses);
+        //console.log(outputLicenses.join('"\n"'));
+        //${licenses.join(', ')}
+       return outputLicenses.join('\n')
+    };
+
      const genereateTips = tips => {
-       
+        // console.log(tips)
 
 return `
 ${tips
@@ -41,7 +184,6 @@ ${tips
 `;
 }         
     return `
-
 ## ${projectTitle}
 1. [ Description. ](#desc)
 2. [ Web Address. ](#web-address)
@@ -54,8 +196,10 @@ ${tips
 9. [ Contributing. ](#contributing)
 9. [ Questions. ](#questions)
 ### For license description, click on the badge to get more information.
-
-
+${licensesBadges(licenses)}
+<a name="desc"></a>
+## 1. Description
+### Top technology used:  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
 ${desc}
 <a name="web-address"></a>
 ## 2. How to Get There
